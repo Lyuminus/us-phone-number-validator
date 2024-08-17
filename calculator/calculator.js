d8c05f6edca6b66c9d199486936cb9214cfca702
Lyuminus <aliaskarkurbonaliev476@gmail.com>
2024-08-17 07:29:46 +0300

    rgghfx

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,24 @@
-// js basics
-// problems
-// 
-
-function check() {
+function c1() {
     let n1 = Number(document.getElementById('number1').value);
-    let n2 = Number(document.getElementById('number2').value);
-    // console.log(n1 + n2);    
-    document.getElementById('answer').innerText += " " + String(n1 + n2); 
+    let n2 = Number(document.getElementById('number2').value);  
+    document.getElementById('answer').innerText += String(n1 + n2) + ";"; 
 }
 
+function c2() {
+    let n1 = Number(document.getElementById('number1').value);
+    let n2 = Number(document.getElementById('number2').value);  
+    document.getElementById('answer').innerText += String(n1 - n2) + ";"; 
+}
+function c3() {
+    let n1 = Number(document.getElementById('number1').value);
+    let n2 = Number(document.getElementById('number2').value);  
+    document.getElementById('answer').innerText += String(n1 * n2) + ";"; 
+}
+function c4() {
+    let n1 = Number(document.getElementById('number1').value);
+    let n2 = Number(document.getElementById('number2').value);  
+    document.getElementById('answer').innerText += String(n1 / n2) + ";"; 
+}
+function c5() {
+    document.getElementById("answer").innerText = " ";
+}
