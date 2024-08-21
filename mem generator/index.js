07935356ddf5a67b21f85c387423a80926c73465
Lyuminus <aliaskarkurbonaliev476@gmail.com>
2024-08-21 15:09:35 +0300

    Fronted 2

---
+++ mem generator/index.js
@@ -0,0 +1,46 @@
+const apiUrl = 'https://meme-api.com/gimme';
+let image = '';
+
+async function newMeme1() {
+    await fetch(`https://meme-api.com/gimme/russian_memes_only`).then((response) => response.json()).then((data) => {
+        image = data.url;
+        console.log(data);
+        ;
+    });
+    document.getElementById("meme1").setAttribute('src', image);
+}
+async function newMeme2() {
+    await fetch(`https://meme-api.com/gimme/MinecraftMemes`).then((response) => response.json()).then((data) => {
+        image = data.url;
+        console.log(data);
+        ;
+    });
+    document.getElementById("meme1").setAttribute('src', image);
+}
+async function newMeme3() {
+    await fetch(`https://meme-api.com/gimme/memes`).then((response) => response.json()).then((data) => {
+        image = data.url;
+        console.log(data);
+        ;
+    });
+    document.getElementById("meme1").setAttribute('src', image);
+}
+async function newMeme4() {
+    await fetch(`https://meme-api.com/gimme/WitcherMemes`).then((response) => response.json()).then((data) => {
+        image = data.url;
+        console.log(data);
+        ;
+    });
+    document.getElementById("meme1").setAttribute('src', image);
+}
+async function newMeme5() {
+    await fetch(`https://meme-api.com/gimme/AniMem`).then((response) => response.json()).then((data) => {
+        image = data.url;
+        console.log(data);
+        ;
+    });
+    document.getElementById("meme1").setAttribute('src', image);
+}
+
+// let city = '';
+// let baseUrl = 'http://api.weatherapi.com/v1/current.json?key=cd3972bb11ff450fac8120003242108&q=' + city +'&aqi=no';
